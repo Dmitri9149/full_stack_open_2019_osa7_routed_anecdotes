040d07b39625a7c5105af4b4d9ad44b592287225
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-06-23 12:43:49 +0300

    components are moved to /components

---
+++ src/components/Menu.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import AnecdoteList from './AnecdoteList'
+import About from './About'
+import CreateNew from './CreateNew'
+
+
+import {
+  BrowserRouter as Router,
+  Route, Link
+} from 'react-router-dom'
+
+const Menu = (props) => {
+  const padding = {
+    paddingRight: 5
+  }
+  return (
+    <div>
+    <Router>
+      <div>
+        <div>
+          <Link style={padding} to="/">anecdotes</Link>
+          <Link style={padding} to="/create">create</Link>
+          <Link style={padding} to="/about">about</Link>
+        </div>
+        <Route exact path="/" render={() => <AnecdoteList anecdotes = {props.anecdotes}/>} />
+        <Route path="/create" render={() => <CreateNew addNew = {props.addNew}/>} />
+        <Route path="/about" render={() => <About />} />
+      </div>
+    </Router>
+  </div>
+  )
+}
+
+export default Menu
